a81e0247714ffa962d8d03531735203ea5b860e7
Murasakaaa <119632356+Murasakaaa@users.noreply.github.com>
2023-11-28 19:15:49 +0100

    Update main.py

--- YoutubeDownloader/main.py
+++ YoutubeDownloader/main.py
@@ -3,7 +3,7 @@
 
 from pytube import YouTube
 
-# url = "https://www.youtube.com/watch?v=Rd8ap3GcAyA&ab_channel=Gawx2"
+url = "https://www.youtube.com/watch?v=Rd8ap3GcAyA&ab_channel=Gawx2"
 
 
 # check if the url begin with "https://www.youtube.com"
@@ -11,7 +11,7 @@ from pytube import YouTube
 # Also if the url is only "https://www.youtube.com", it will ask you the url again
 
 base_ytb_url = "https://www.youtube.com"
-while True:
+'''while True:
     url = input("url of the video you want to download: ")
     # if url[:len(base_ytb_url)] == base_ytb_url:
     if url == base_ytb_url:
@@ -19,7 +19,7 @@ while True:
     elif url.lower().startswith(base_ytb_url):
         break
     print("The url is not valid. You have to enter a YouTube url.")
-
+'''
 
 def on_download_progress(stream, chunk, bytes_remaining):
     """take the filesize - bytes remaining, convert it to percents
@@ -37,12 +37,38 @@ youtube_video.register_on_progress_callback(on_download_progress)  # calls the f
 print("Video title:", youtube_video.title)  # show the title of the video
 print("Views:", youtube_video.views)  # show the number of views of the video
 
-print("Streams")
-for stream in youtube_video.streams:  # show all the streams of the video
-    print(" ", stream)
+print("")
+print("CHOICE OF RES")
+streams = youtube_video.streams.filter(progressive=True, file_extension='mp4')
+index = 1
+for stream in streams:  # show all the streams of the video
+    print(f'{index} - {stream.resolution}')
+    index +=1
 
-# stream = youtube_video.streams.get_by_itag(18) # for a specific stream
-stream = youtube_video.streams.get_highest_resolution()  # to get the stream with the highest res
+# We make sure the user is entering the right number
+while True:
+    res_num = input("Choose a resolution: ")
+    if res_num == "":
+        print("Error. Please enter a number.")
+    else:
+        try:
+            res_num_int = int(res_num)
+        except:
+            print("Error. Please enter a number.")
+        else:
+            if not 1 <= res_num_int <= len(streams):
+                print(f'Error. You have to enter a number between 1 and {len(streams)}')
+            else:
+                break
+
+
+
+itag = streams[res_num_int-1].itag
+
+# print("itag: ", itag)
+
+stream = youtube_video.streams.get_by_itag(itag)  # for a specific stream
+# stream = youtube_video.streams.get_highest_resolution()  # to get the stream with the highest res
 print("Downloading...")
 
 stream.download()  # downloading the stream
