720e982580533e89d75ee8a320b5b920f16ff09a
Murasakaaa <119632356+Murasakaaa@users.noreply.github.com>
2024-08-01 19:40:00 +0200

    New features
    - added the possibility to choose between downloading mp3 or mp4.
    - added the same program but with tkinter

---
+++ YoutubeDownloader/with_tkinter.py
@@ -0,0 +1,138 @@
+import tkinter as tk
+from tkinter import ttk
+from pytubefix import YouTube
+import os
+import ffmpeg
+
+# create the window
+window = tk.Tk()
+window.title('YTDownloader')
+window.geometry('240x240')
+window.resizable(width=False, height=False)
+
+choice = 0
+def audio_only():
+    global choice
+    choice = 1
+    current_choice.config(text="Current choice: mp3")
+
+def normal_vid():
+    global choice
+    choice = 2
+    current_choice.config(text="Current choice: mp4")
+
+def dl_video():
+    process(choice)
+
+def process(choice):
+    url = enter_url.get()
+    vid = YouTube(url)
+    label2.config(text=f"title: {vid.title}")
+    label3.config(text=f"views: {vid.views}")
+    c = choice
+
+    if c == 1:
+        progress['value'] = 0
+        stream = vid.streams.filter(only_audio=True).order_by('abr').desc()
+        vid_stream = stream[0]
+        print(vid_stream)
+        progress['value'] = 50
+        vid.register_on_progress_callback(on_download_progress)
+        vid_stream.download(mp3=True)
+        print('finished')
+        progress['value'] = 100
+
+    if c == 2:
+        progress['value'] = 0
+        progress['value'] = 10
+        vid.register_on_progress_callback(on_download_progress)
+        streams = vid.streams.filter(progressive=False, file_extension='mp4', type="video").order_by('resolution').desc()
+        video_stream = streams[0]
+        streams = vid.streams.filter(progressive=False, file_extension='mp4', type="audio").order_by('abr').desc()
+        audio_stream = streams[0]
+
+        progress['value'] = 30
+        print("Video downloading...")
+        video_stream.download("video")  # downloading the video part in the "video" folder
+        print("finished")
+
+        progress['value'] = 50
+        print("Audio downloading...")
+        audio_stream.download("audio")  # downloading the audio part in the "audio" folder
+        print("finished")
+
+        # --- Combining the two files into 1 video with audio ---
+        # ffmpeg can't use streams as it is a pytube thing, so it uses filenames
+        audio_filename = os.path.join("audio", video_stream.default_filename)
+        video_filename = os.path.join("video", video_stream.default_filename)
+        output_filename = video_stream.default_filename
+        progress['value'] = 70
+        print("Creating the final file...")
+        ffmpeg.output(ffmpeg.input(audio_filename), ffmpeg.input(video_filename), output_filename, vcodec="copy",acodec="copy", loglevel="quiet").run(overwrite_output=True)
+        print("finished")
+        progress['value'] = 80
+
+        progress['value'] = 85
+        print('Deleting temporary files...')
+        progress['value'] = 90
+        os.remove(audio_filename)
+        progress['value'] = 95
+        os.remove(video_filename)
+        print('finished')
+        progress['value'] = 100
+
+def on_download_progress(stream, chunk, bytes_remaining):
+    """take the filesize - bytes remaining, convert it to percents
+    and show the percentage of download progress"""
+
+    bytes_downloaded = stream.filesize - bytes_remaining
+    percent = bytes_downloaded * 100 / stream.filesize
+    print(f"Download Progress: {int(percent)}%")
+
+window.columnconfigure(0, weight=2)
+window.columnconfigure(1, weight=1)
+window.columnconfigure(2, weight=1)
+window.columnconfigure(5, weight=2)
+window.columnconfigure(6, weight=2)
+# widgets
+label0 = ttk.Label(text='mp3 or mp4?')
+label0.grid(row=0, column=0, pady=5, padx=5, sticky='w')
+
+# buttons to choose to download audio only or normal videos
+audio_choice = ttk.Button(window, text="mp3",command=audio_only)
+audio_choice.grid(row=1,column=0, pady=5, padx=5)
+
+video_choice = ttk.Button(window, text="mp4",command=normal_vid)
+video_choice.grid(row=1,column=1, pady=5, padx=5)
+
+# show the last choice you made with the buttons above
+current_choice = ttk.Label(window,text="Current choice : ")
+current_choice.grid(column=0, row=2, columnspan=3, sticky='w')
+
+ttk.Separator(window, orient='horizontal').place(x=0, y=85, relwidth=1)
+
+
+label1 = ttk.Label(text="Video link:")
+label1.grid(column=0, row=3, pady=5, padx=5, sticky='w')
+
+# this is where you enter the video url
+enter_url = ttk.Entry(window)
+enter_url.grid(column=0, row=4, pady=5, padx=5)
+
+# download button
+dl_button = ttk.Button(window, text='download', command=dl_video)
+dl_button.grid(column=1, row=4, pady=5, padx=5)
+
+# video infos
+# title
+label2 = ttk.Label(text="title: ")
+label2.grid(column=0, row=5, padx=5, sticky='w', columnspan=3)
+# number of views
+label3 = ttk.Label(text="views: ")
+label3.grid(column=0, row=6, padx=5, sticky='w', columnspan=3)
+
+# download status
+progress = ttk.Progressbar(window, orient='horizontal', length=200, mode='determinate')
+progress.place(x=20, y=200)
+# run
+window.mainloop()
---
+++ YoutubeDownloader/without_tkinter.py
@@ -0,0 +1,87 @@
+# YouTube downloader -> can't download age restricted video bcause you have to log in to yt
+
+from pytubefix import YouTube
+import os
+import ffmpeg
+
+base_ytb_url = "https://www.youtube.com"
+
+# check if the url begin with "https://www.youtube.com"
+# if not then it shows an error and ask you the url again
+# Also if the url is only "https://www.youtube.com", it will ask you the url again
+def get_video_url_from_user():
+
+    while True:
+        u = input("url of the video you want to download: ")
+        if u == base_ytb_url:  # if the url is YouTube's url it is not working
+            print(".")
+        elif u.lower().startswith(base_ytb_url):  # if u[:len(base_ytb_url)] == base_ytb_url:
+            return u
+        print("The url is not valid. You have to enter a YouTube url.")
+
+def choice():
+    while True:
+        choice = int(input("mp3(1) or mp4(2)?: "))
+        if choice == 1 or choice == 2:
+            return choice
+        print('enter correct choice')
+
+def dl_video():
+    url = get_video_url_from_user()
+    vid = YouTube(url)
+    print("Video title: ", vid.title)  # show the title of the video
+    c = choice()
+    if c == 1:
+        print('downloading...')
+        stream = vid.streams.filter(only_audio=True).order_by('abr').desc()
+        vid_stream = stream[0]
+        print(vid_stream)
+        vid.register_on_progress_callback(on_download_progress)
+        vid_stream.download(mp3=True)
+        print('finished')
+
+    if c == 2:
+        vid.register_on_progress_callback(on_download_progress)
+        streams = vid.streams.filter(progressive=False, file_extension='mp4', type="video").order_by('resolution').desc()
+        video_stream = streams[0]
+
+        streams = vid.streams.filter(progressive=False, file_extension='mp4', type="audio").order_by('abr').desc()
+        audio_stream = streams[0]
+        print("Video downloading...")
+        video_stream.download("video")  # downloading the video part in the "video" folder
+        print("finished")
+
+        print("")
+
+        print("Audio downloading...")
+        audio_stream.download("audio")  # downloading the audio part in the "audio" folder
+        print("finished")
+
+        print("")
+
+        # --- Combining the two files into 1 video with audio ---
+        # ffmpeg can't use streams as it is a pytube thing, so it uses filenames
+        audio_filename = os.path.join("audio", video_stream.default_filename)
+        video_filename = os.path.join("video", video_stream.default_filename)
+        output_filename = video_stream.default_filename
+
+        print("Creating the final file...")
+        ffmpeg.output(ffmpeg.input(audio_filename), ffmpeg.input(video_filename), output_filename, vcodec="copy",acodec="copy", loglevel="quiet").run(overwrite_output=True)
+        print("finished")
+
+        print("")
+
+        print('Deleting temporary files...')
+        os.remove(audio_filename)
+        os.remove(video_filename)
+        print('finished')
+
+def on_download_progress(stream, chunk, bytes_remaining):
+    """take the filesize - bytes remaining, convert it to percents
+    and show the percentage of download progress"""
+
+    bytes_downloaded = stream.filesize - bytes_remaining
+    percent = bytes_downloaded * 100 / stream.filesize
+    print(f"Download Progress: {int(percent)}%")
+
+dl_video()
